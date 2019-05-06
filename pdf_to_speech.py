0b91dea11d9d2067ee8df9c71ad9a5a6e12f45e4
stuphys1729 <s1432366@sms.ed.ac.uk>
2019-05-06 11:52:27 +0100

    Initial commit with pdf to speech tool

---
+++ pdf_to_speech.py
@@ -0,0 +1,114 @@
+"""Synthesizes speech from the input string of text or ssml.
+
+Note: ssml must be well-formed according to:
+    https://www.w3.org/TR/speech-synthesis/
+"""
+from google.cloud import texttospeech
+import PyPDF2
+import os
+import sys
+import time
+from google.api_core.exceptions import InvalidArgument
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+class PdfConverter:
+
+   def __init__(self, file_path):
+       self.file_path = file_path
+# convert pdf file to a string which has space among words 
+   def convert_pdf_to_txt(self):
+       start = time.time()
+       rsrcmgr = PDFResourceManager()
+       retstr = StringIO()
+       codec = 'utf-8'  # 'utf16','utf-8'
+       laparams = LAParams()
+       device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+       fp = open(self.file_path, 'rb')
+       interpreter = PDFPageInterpreter(rsrcmgr, device)
+       password = ""
+       maxpages = 0
+       caching = True
+       pagenos = set()
+       for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
+           interpreter.process_page(page)
+           retstr.write("PAGEBREAKER")
+       fp.close()
+       fullstr = retstr.getvalue()
+       device.close()
+       retstr.close()
+
+       print("Processed pdf in {:.2f}s".format(time.time() - start))
+       return fullstr.split("PAGEBREAKER")
+
+# convert pdf file text to string and save as a text_pdf.txt file
+   def save_convert_pdf_to_txt(self):
+       content = self.convert_pdf_to_txt()
+       txt_pdf = open('text_pdf.txt', 'wb')
+       txt_pdf.write(content.encode('utf-8'))
+       txt_pdf.close()
+
+def main(file_name=None, start_page=None, end_page=None):
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    if not file_name:
+        file_name = 'RLbook2018.pdf'
+
+    directory = file_name.rstrip(".pdf")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    pdfConverter = PdfConverter(file_name)
+    pages = pdfConverter.convert_pdf_to_txt()
+
+    for p, page in enumerate(pages):
+
+        if start_page and end_page and p not in range(int(start_page)-1, int(end_page)):
+            continue
+
+        ## DEBUG ##
+        page_content = "".join(i for i in page[:-1].replace('"','').replace('\x00','') if ord(i) < 128)
+
+        if len(page_content) > 5000:
+            page_content = page_content[:5000]
+        # Set the text input to be synthesized
+        synthesis_input = texttospeech.types.SynthesisInput(text=page_content)
+
+        voice = texttospeech.types.VoiceSelectionParams(
+            language_code='en-US',
+            # ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL,
+            name="en-US-Wavenet-A")
+
+        # Select the type of audio file you want returned
+        audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.MP3,
+            speaking_rate=1)
+
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        try:
+            response = client.synthesize_speech(synthesis_input, voice, audio_config)
+        except InvalidArgument as e:
+            print(repr(page_content))
+            print("Section is {} characters long".format(len(page_content)))
+            raise(e)
+
+        # The response's audio_content is binary.
+        new_file = directory + '/page' + str(p+1) + ".mp3"
+        with open(new_file, 'wb') as out:
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            print('Audio content written to file "' + new_file + '"')
+
+        new_file = new_file.rstrip(".mp3") + "_script.txt"
+        with open(new_file, 'wb') as out:
+            out.write(page_content.encode('utf-8'))
+
+if __name__ == "__main__":
+
+    main(*sys.argv[1:])
